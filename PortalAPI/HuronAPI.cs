a05c6cb5503420f0983ecafbaebe451a72e28ca8
Brandon Powers <brandopowers@gmail.com>
2026-05-05 10:36:50 -0600

    Added method summary and parameter descriptions.

--- PortalAPI/HuronAPI.cs
+++ PortalAPI/HuronAPI.cs
@@ -6,6 +6,13 @@ namespace PortalAPI
 {
     public class HuronAPI
     {
+        /// <summary>
+        /// GETs data from a Portal WebServices API.
+        /// </summary>
+        /// <param name="domain">Domain of the application</param>
+        /// <param name="apiPath">Name of the WebService API</param>
+        /// <param name="credential">base64 encoded username:password</param>
+        /// <returns></returns>
         public static HuronAPIData GetAPIData(string domain, string apiPath, string credential) {
             var client = new HttpClient();
             var request = new HttpRequestMessage(HttpMethod.Get, $"https://{domain}/api/click/datamanagement/{apiPath}");
@@ -24,7 +31,6 @@ namespace PortalAPI
                 .GetAwaiter()
                 .GetResult();
 
-            Console.WriteLine(body);
             var data = JsonSerializer.Deserialize<HuronAPIData>(body);
 
             return data;
